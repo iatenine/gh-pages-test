fa6e6dea11b7e8f15195645650e58e546912e04d
iatenine <iatenine@gmail.com>
2022-10-01 11:08:05 -0500

    Adapt project to work with GitHub Pages and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const Main = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload. <br />
+        Click <Link to="/example">here</Link> to see another route
+      </p>
+    </header>
+  </div>
+);
+
+const Example = () => <div>This is an example</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={`${process.env.PUBLIC_URL}`}>
+      <Routes>
+        <Route path="/example" element={<Example />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
